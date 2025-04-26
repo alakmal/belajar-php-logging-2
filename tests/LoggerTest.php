8c1eba8ef77e68c83a4c80bd448448fb0f08e146
alyouma <alyoumaakmal4@gmail.com>
2025-04-26 14:12:56 +0700

    processor'

--- tests/LoggerTest.php
+++ tests/LoggerTest.php
@@ -4,6 +4,8 @@ namespace Alyou\Belajar\Php\Logging2;
 
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
+use Monolog\Processor\GitProcessor;
+use Monolog\Processor\MemoryUsageProcessor;
 
 class LoggerTest extends \PHPUnit\Framework\TestCase
 {
@@ -13,9 +15,35 @@ class LoggerTest extends \PHPUnit\Framework\TestCase
         $logger->pushHandler(new StreamHandler(("php://stdout")));
         $logger->pushHandler(new StreamHandler(__DIR__ . "/../error.log"));
 
-        $logger->info("Info message", ["username" => "Khnnedy"]);
-        $logger->warning("Warning message", ["password" => "Khnnedy"]);
+        $logger->pushProcessor(function ($record) {
+            $record['extra']['pzn'] = [
+                "author" => "Alyou",
+                "application" => "Belajar PHP Logging",
+            ];
 
+            return $record;
+        });
+        $logger->info(("test processor"));
+        self::assertNotNull($logger);
+    }
+
+    public function testProcessorMonolog()
+    {
+        $logger = new Logger(LoggerTest::class);
+        $logger->pushHandler(new StreamHandler(("php://stdout")));
+        $logger->pushHandler(new StreamHandler(__DIR__ . "/../application.log"));
+        $logger->pushProcessor(new GitProcessor());
+        $logger->pushProcessor(new MemoryUsageProcessor());
+        $logger->pushProcessor(function ($record) {
+            $record['extra']['pzn'] = [
+                "author" => "Alyou",
+                "application" => "Belajar PHP Logging",
+            ];
+
+            return $record;
+        });
+
+        $logger->info("test processor", ["context" => "this is a context"]);
         self::assertNotNull($logger);
     }
 }
