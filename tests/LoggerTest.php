30f2f57c883d0a979a0546e9f8e32e6721738ab7
alyouma <alyoumaakmal4@gmail.com>
2025-04-26 09:00:59 +0700

    logger

---
+++ tests/LoggerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use Monolog\Logger;
+
+class LoggerTest extends \PHPUnit\Framework\TestCase
+{
+    public function testLogMessage()
+    {
+        $logger = new Logger("ProgrammeZamanNow");
+
+        self::assertNotNull($logger);
+
+
+        // // Check if the log file exists and contains the expected message
+        // $this->assertFileExists('app.log');
+        // $this->assertStringContainsString("Test message", file_get_contents('app.log'));
+    }
+}
